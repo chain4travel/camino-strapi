9c6353e1e72ade89d17526510c3e6e05706ec2bb
Strapi Machine <mohamed.elmoslemany@chain4travel.com>
2023-02-17 14:00:50 +0000

    initial commit

---
+++ src/api/business-field/controllers/business-field.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * business-field controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::business-field.business-field');
---
+++ src/api/business-field/routes/business-field.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * business-field router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::business-field.business-field');
